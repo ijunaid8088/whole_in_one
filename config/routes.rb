8924ddeb50174936446ef681172db46463eb382a
Junaid Farooq <junaid@evercam.io>
2017-02-04 17:06:17 +0500

    Added routes and controller indexs.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'index#welcome'
+  get 'sign_in' => 'index#sign_in'
+  get 'sign_up' => 'index#sign_up'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
