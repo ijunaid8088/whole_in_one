745a031c6a4d651790adf67f94e95d92f8e898b5
Junaid Farooq <junaid@evercam.io>
2017-02-05 22:05:27 +0500

    Added Company route.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'sign_in' => 'index#sign_in'
   get 'sign_up' => 'index#sign_up'
   get "dashboard" => "dashboard#index"
+  get "company" => "company#index"
 
   put "/admin/session" => "admin#log_in"
   get "/admin/log_out" => "admin#log_out"
