bef0a067698714d56965946584c9cb49d947969c
Junaid Farooq <junaid@evercam.io>
2017-02-05 11:29:35 +0500

    Added current_user for usage.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def current_user
-    return unless session[:admin_id]
-    Admin.find(session[:admin_id])
-  end
+  include ApplicationHelper
+  helper_method :current_user
 
   def authorize_admin
     redirect_to sign_in_path if !session[:admin_id].present?
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_user
+    Admin.find(session[:admin_id])
+  end
 end
