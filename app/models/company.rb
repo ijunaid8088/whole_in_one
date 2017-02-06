11b0cb69a8620a143f55a25940fcd9647d4e2009
Junaid Farooq <junaid@evercam.io>
2017-02-06 18:29:42 +0500

    Added company model with valid namespace regex.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ActiveRecord::Base
   belongs_to :admin
+  validates :namespace, presence: true, uniqueness: true
+  validates :company_name, presence: true
+  validates_format_of :namespace, :with => /\A(?:http(?:s)?:\/\/)?(?:www\.)?(?:[\w-]*)\.\w{2,}\z/
 end
