44826bbabfe1623106705dcd6a6f0b32cc207be6
Junaid Farooq <junaid@evercam.io>
2017-02-06 18:29:24 +0500

    Companies controller with create method.

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,4 +1,25 @@
 class CompanyController < ApplicationController
+
   def index
+  end
+
+  def create
+    begin
+      @company = Company.new(
+        company_name: params[:company_name],
+        namespace: params[:namespace],
+        admin_id: params[:admin_id]
+      )
+      respond_to do |format|
+        if @company.save
+          format.json { render json: @company.to_json() }
+          Rails.logger.info "Company has been created."
+        else
+          format.json { render json: @company.errors.full_messages.first, status: :unprocessable_entity }
+        end
+      end
+    rescue => error
+      Rails.logger.error error
+    end
   end
 end
