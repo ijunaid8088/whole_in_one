43fd3137acdfa28186ce02cd315401318ffe233c
Junaid Farooq <junaid@evercam.io>
2017-02-18 10:41:34 +0500

    Migration ran on schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206064431) do
+ActiveRecord::Schema.define(version: 20170218053815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 20170206064431) do
   end
 
   add_index "companies", ["admin_id"], name: "index_companies_on_admin_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "password"
+    t.integer  "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "companies", "admins"
 end
