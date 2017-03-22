cb2edcf320a9c3557243765d29a66192682a7dae
Junaid Farooq <junaid@evercam.io>
2017-03-22 22:45:20 +0500

    Updated Gems.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -32,9 +31,8 @@ ActiveRecord::Schema.define(version: 20170225014648) do
     t.integer  "admin_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.index ["admin_id"], name: "index_companies_on_admin_id", using: :btree
   end
-
-  add_index "companies", ["admin_id"], name: "index_companies_on_admin_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
